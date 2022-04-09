a391cac56448c1ac751813034106664046e48cc9
mc-b <marcel.bernet@ch-open.ch>
2022-04-09 11:40:51 +0200

    Output ip und fqdn

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
+###
+#   Outputs wie IP-Adresse und DNS Name
+#
+
 output "ip_vm" {
   value = aws_instance.vm.public_ip
+  description = "The IP address of the AWS server instance."
+  
+}
+
+output "ip_fqdn" {
+  value = aws_instance.vm.public_dns
+  description = "The FQDN of the AWS server instance."
+  
 }
