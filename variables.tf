dd340142d8e003e77f1ed5123abee13d7b81ce51
mc-b <marcel.bernet@ch-open.ch>
2022-04-13 15:03:48 +0200

    Abhandlung Instance Type

--- variables.tf
+++ variables.tf
@@ -18,6 +18,23 @@ variable "ports" {
     default = [ 22, 80 ]
 }
 
+variable "mem" {
+    type    = string
+    default = "1GB"
+}
+
+# Umwandlung "mem" nach AWS Instance Type
+
+variable "instance_type" {
+  type = map
+  default = {
+    "1GB" = "t2.micro"
+    "2GB" = "t2.small"
+    "4GB" = "t2.medium"
+    "8GB" = "t2.large"
+  }
+}
+
 # Scripts
 
 data "template_file" "userdata" {
