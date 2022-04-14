10f1331efc4fdceed6f8034b6ca38d292ccf56a7
mc-b <marcel.bernet@ch-open.ch>
2022-04-14 11:31:25 +0200

    Abhandlung Dynamic Ports

--- main.tf
+++ main.tf
@@ -7,21 +7,25 @@
 resource "aws_security_group" "security" {
   name        = var.module
 
-  # SSH access from anywhere
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.ports 
+      content {
+            from_port   = ingress.value
+            to_port     = ingress.value
+            protocol    = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]      
+      } 
   }
-
-  # HTTP access from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  
+  dynamic "ingress" {
+    for_each = var.ports 
+      content {
+            from_port   = ingress.value
+            to_port     = ingress.value
+            protocol    = "udp"
+            cidr_blocks = ["0.0.0.0/0"]      
+      } 
+  }  
   
   # All other from myip (tcp)
   ingress {
